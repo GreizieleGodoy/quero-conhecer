aa288b1747cadae6f52a93e91f67c95272c065b8
Greiziele Godoy <greizielegodoy@MacBook-Air-de-Gabriel.local>
2021-07-05 13:48:04 -0300

    Project Done

---
+++ QueroConhecer/PlaceAnnotation.swift
@@ -0,0 +1,28 @@
+//
+//  PlaceAnnotation.swift
+//  QueroConhecer
+//
+//  Created by Greiziele Godoy on 17/06/21.
+//
+
+import Foundation
+import MapKit
+
+enum PlaceType {
+    case place
+    case poi
+}
+
+class PlaceAnnotation: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    var type: PlaceType
+    var address: String?
+    
+    init(coordinate: CLLocationCoordinate2D, type: PlaceType) {
+        self.coordinate = coordinate
+        self.type = type
+    }
+    
+}
